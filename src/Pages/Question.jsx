e6f8a2fc156e7f6c04a0e626cf29c58b38788100
sriramk1n1 <sriramjkini@gmail.com>
2024-05-09 22:55:17 +0530

    made responsive

--- src/Pages/Question.jsx
+++ src/Pages/Question.jsx
@@ -96,27 +96,30 @@ const [code,setcode]=useState(null);
         <MediaQuery query="(max-device-width: 1024px)">
         <PanelGroup direction="vertical">
           <Panel className='mx-8'>
-            <div className='p-3'>
+          <div className='p-3 overflow-y-auto max-h-[36vh]'>
               <h1 key="1"  className='pt-2 pb-2'>{arr && arr.name}</h1>
               <div>{arr && arr.desc.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
               <h4 key="2" className='pt-5 pb-2'>Constraints:</h4>
               <div>{arr && arr.constraints.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
-              <h4 key="3" className='pt-5 pb-2'>Input</h4>
-              <div>{arr && arr.input1.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
-              <h4 key="4" className='pt-5 pb-2'>Output</h4>
-              <div>{arr && arr.output1.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
-              <div>{arr && arr.input2.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
-              <h4 key="4" className='pt-5 pb-2'>Output</h4>
-              <div>{arr && arr.output2.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
-              <div>{arr && arr.input3.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
-              <h4 key="4" className='pt-5 pb-2'>Output</h4>
-              <div>{arr && arr.output3.split("\n").map((ele,idx)=>(<div key={idx}>{ele}</div>))}</div>
+              <h4 className='pt-5 pb-2'> {"\nSample Testcase:"} </h4>
+              <h3 key="3" className='pt-3 pb-0'>{arr && arr.input1 && "Input:"}</h3>
+              <div   className={`border-l-3 ${props.toggle.dark? "bg-gray-800" : "bg-gray-200 border-gray-400"}`}>{arr && arr.input1.split("\n").map((ele,idx)=>(<div  className="pl-2 whitespace-pre" key={idx}>{ele}</div>))}</div>
+              <h3 key="4" className='pt-0 pb-0'>{arr && arr.output1 && "Output:"}</h3>
+              <div  className={`border-l-3 ${props.toggle.dark? "bg-gray-800" : "bg-gray-200 border-gray-400"}`}>{arr && arr.output1.split("\n").map((ele,idx)=>(<div className="pl-2 whitespace-pre"  key={idx}>{ele}</div>))}</div>
+              <h3 key="3" className='pt-3 pb-0'>{arr && arr.input2 && "Input:"}</h3>
+              <div  className={`border-l-3 ${props.toggle.dark? "bg-gray-800" : "bg-gray-200 border-gray-400"}`}>{arr && arr.input2.split("\n").map((ele,idx)=>(<div  className="pl-2 whitespace-pre" key={idx}>{ele}</div>))}</div>
+              <h3 key="5" className='pt-0 pb-0'>{arr && arr.input2 && "Output:"}</h3>
+              <div  className={`border-l-3 ${props.toggle.dark? "bg-gray-800" : "bg-gray-200 border-gray-400"}`}>{arr && arr.output2.split("\n").map((ele,idx)=>(<div className="pl-2 whitespace-pre"  key={idx}>{ele}</div>))}</div>
+              <h3 key="3" className='pt-3 pb-0'>{arr && arr.input3 && "Input:"}</h3>
+              <div  className={`border-l-3 ${props.toggle.dark? "bg-gray-800" : "bg-gray-200 border-gray-400"}`}>{arr && arr.input3.split("\n").map((ele,idx)=>(<div  className="pl-2 whitespace-pre" key={idx}>{ele}</div>))}</div>
+              <h3 key="6" className='pt-0 pb-0'>{arr && arr.input3 && "Output:"}</h3>
+              <div  className={`border-l-3 ${props.toggle.dark? "bg-gray-800" : "bg-gray-200 border-gray-400"}`}>{arr && arr.output3.split("\n").map((ele,idx)=>(<div className="pl-2 whitespace-pre"  key={idx}>{ele}</div>))}</div>
             </div>
           </Panel>
           <PanelResizeHandle className={`h-1 ${props.toggle.dark? "bg-gray-800" : "bg-blue-50"}`}/>
         
               <Panel>
-                  <Editor language={lang} theme={props.toggle.dark?'vs-dark':'light'} 
+                  <Editor language="cpp" theme={props.toggle.dark?'vs-dark':'light'} 
                   value={com}
                    onChange={(v,e)=>setcode(v)}
                    options={{minimap: { enabled: false }}}
@@ -125,14 +128,40 @@ const [code,setcode]=useState(null);
               </Panel>
               <PanelResizeHandle className={`h-1 ${props.toggle.dark? "bg-gray-800" : "bg-blue-50"}`}/>
               <Panel >
-                 <form onSubmit={subsolution} encType='multipart/form-data'>
-                    <div>
-                      <Button className="my-3" size="sm" onClick={subsolution}
-                      > Submit </Button>
-                    </div>
-                  </form>
-
-                {result && JSON.stringify(result)}
+              {result && result.warnings !== "" && (
+    <ScrollShadow className='pl-5'>
+      <div className="text-red-600">Warnings:</div>
+      {result.warnings.split("\n").map((ele) => (
+        <div>{ele}</div>
+      ))}
+    </ScrollShadow>
+  )}
+  
+  <div className='absolute right-28 ml-auto' >
+      <Button className={`my-3 ${props.toggle.dark? "bg-red-950" : "bg-red-200"}`} size="sm" onClick={skipsolution}>
+        Skip
+      </Button>
+    </div>
+  <form className="absolute right-10" onSubmit={subsolution} encType='multipart/form-data' style={{ marginLeft: 'auto' }}>
+    
+    <div>
+      <Button className="my-3" size="sm" onClick={subsolution}>
+        Submit
+      </Button>
+    </div>
+  </form>
+  {result && result.warnings === "" && result.total === result.passed && (
+    <ScrollShadow className='pl-5'>
+      <div className="text-green-600">Accepted</div>
+      {`Passed ${result.passed}/${result.total} testcases.`}
+    </ScrollShadow>
+  )}
+  {result && result.warnings === "" && result.total !== result.passed && (
+    <ScrollShadow className='pl-5'>
+      <div className="text-grey-600">All testcases are not passed</div>
+      {`Passed ${result.passed}/${result.total} testcases.`}
+    </ScrollShadow>
+  )}
               </Panel>
       
         </PanelGroup>
@@ -172,7 +201,7 @@ const [code,setcode]=useState(null);
           <Panel minSize={20}>
             <PanelGroup direction="vertical"  >
               <Panel minSize={20} className='flex-col'>
-                  <Editor language={lang} theme={props.toggle.dark?'vs-dark':'light'} 
+                  <Editor language="cpp" theme={props.toggle.dark?'vs-dark':'light'} 
                   value={com}
                    onChange={(v,e)=>{setcode(v); setSize(10); setResult(null)}}
                    options={{minimap: { enabled: false }}}
